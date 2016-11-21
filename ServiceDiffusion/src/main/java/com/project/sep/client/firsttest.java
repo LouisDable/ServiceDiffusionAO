e1181fac75e6e50e0627280a16aa20d4d299c91e
LouisDable <Dablelouisqc@gmail.com>
2016-11-21 16:15:52 +0100

    Creation du projet

---
+++ ServiceDiffusion/src/main/java/com/project/sep/client/firsttest.java
@@ -0,0 +1,10 @@
+package com.project.sep.client;
+
+public class firsttest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
