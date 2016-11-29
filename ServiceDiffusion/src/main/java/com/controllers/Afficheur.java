df29b3ba9f5e3ccd1abf0179bb173afeb453617c
evariste <evariste.sie@gmail.com>
2016-11-29 16:04:41 +0100

    Implementation de l'afficheur

--- ServiceDiffusion/src/main/java/com/controllers/Afficheur.java
+++ ServiceDiffusion/src/main/java/com/controllers/Afficheur.java
@@ -1,16 +1,21 @@
 package com.controllers;
 
 import com.interfaces.ISujetObserve;
+import com.vues.VAfficheur;
 
 public class Afficheur {
 	
 	public int idAfficheur;
-	
+	Integer valeurAffiche;
+	VAfficheur afficheur;
+		
 	public Afficheur(int num){
 		idAfficheur = num;
+		afficheur= new VAfficheur(this);
 	}
 	
 	public void actualiseAfficheur(ISujetObserve Observable){
+		
 		
 	}
 
